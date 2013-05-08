394c07cd60d0fbbd559907d5b6cd02596dde5d2e
shrimpboyho <new.joseph10@gmail.com>
2013-05-07 20:18:10 -0400

    Added more stuff

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  var action_url = "javascript:window.print();";
-  chrome.tabs.update(tab.id, {url: action_url});
+ 
+
+  chrome.tabs.executeScript(null,
+                           {code:"alert('ran injceted Script')"});
+  alert("Executed Script");
+
 });
