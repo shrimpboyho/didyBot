44548123202bc5253ca4701ae051c8250f376ddb
shrimpydubsy <new.joseph10@gmail.com>
2013-07-20 14:34:00 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-<a href="javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://shrimpboyho.github.com/didyBot/code.js';})();">DidyBot</a>
+(function(){document.body.appendChild(document.createElement('script')).src='http://shrimpboyho.github.com/didyBot/code.js';})();
