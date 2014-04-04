f1ea686ebd3e92e39a794923b27af944f7a4b8e6
dpwivagg <dpwivagg@gmail.com>
2014-04-04 18:12:34 -0400

    Test

--- code.js
+++ code.js
@@ -39,3 +39,8 @@ press.which = 32;
 $("input.txtInput").val(word);
 
 // $('input.txtInput').simulateKeyPress(' ');
+
+document.onkeypress = function (e) {
+    console.log("Space bar pressed");
+    // use e.keyCode
+};
