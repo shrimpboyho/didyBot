eb90251889990fbaf0e2621918f054177526ad2e
shrimpydubsy <new.joseph10@gmail.com>
2014-04-04 13:28:28 -0400

    Update code.js

--- code.js
+++ code.js
@@ -10,6 +10,15 @@ console.log("Bot Running...");
 	// Grab the element
 	var word = $('span[style*="text-decoration: underline; color: rgb(153, 204, 0);"]').text();
 	console.log(word);
-	$('input.txtInput').val(word);
+	var i;
+	for(i = 0; i < word.length; i++){
+		// simulate the typing of each key
+		var press = jQuery.Event("keypress");
+		press.ctrlKey = false;
+		press.which = word.charCodeAt(i);
+		$('input.txtInput').trigger(press);
+	}
+	
+	
 //}
 			
