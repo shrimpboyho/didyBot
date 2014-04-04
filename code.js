a76f50e601f305833d9aa0dddc71d499c8f6d508
shrimpydubsy <new.joseph10@gmail.com>
2014-04-04 13:00:25 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,34 +1,8 @@
-(function(){
+console.log("Bot Running...");
 
-	// the minimum version of jQuery we want
-	var v = "1.9.1";
-
-	// check prior inclusion and version
-	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-		var done = false;
-		var script = document.createElement("script");
-		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-		script.onload = script.onreadystatechange = function(){
-			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-				done = true;
-				initMyBookmarklet();
-			}
-		};
-		document.getElementsByTagName("head")[0].appendChild(script);
-	} else {
-		initMyBookmarklet();
-	}
-	
-	function initMyBookmarklet() {
-		(window.myBookmarklet = function() {
-			console.log("Bot Running...");
-
-			while(true){
-				// Grab the element
-				var word = $('span[style*="text-decoration: underline; color: rgb(153, 204, 0);"]').val();
-				console.log(word);
-			}
-		})();
-	}
-
-})();
+while(true){
+	// Grab the element
+	var word = $('span[style*="text-decoration: underline; color: rgb(153, 204, 0);"]').val();
+	console.log(word);
+}
+			
