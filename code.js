d1a30bb1f37e44304f32b4e31b461a5cf41085d9
shrimpydubsy <new.joseph10@gmail.com>
2014-04-04 12:54:04 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,50 +1,10 @@
 console.log("Bot Running...");
 
-/*  BOT PHRASES */
-
-var phrases = [
-  
-  "JQUERY RULES!",
-  "PHP RULES!",
-  "I LIKE CAKE!"
-  
-];
-
-var PHRASE_COUNT = 2;
-
-$.get('http://www.random.org/sequences/?min=0&max=2&col=1&format=plain&rnd=new', function(data) {
-
-  	botLoop(data.split("\n"));	
-	console.log(data.split("\n"));
-
-});
-
-function botLoop(randomNums){
-  
-  // Message every thirty minutes
-  var i = 0;
-  
-  setInterval(function(){
-    
-    $('#input').val(phrases[randomNums[i]]);
-    sendMessage();
-      
-    if(i != PHRASE_COUNT)
-      i++;
-    if(i == PHRASE_COUNT)
-      i = 0;
-      
-  }, minutes(1));
+while(true){
+	
+	// Grab the element
+	var word = $('span[style*="text-decoration: underline; color: rgb(153, 204, 0);"]').val();
+	console.log(word);
+	
+	
 }
-
-function minutes(time){
-	return time * 60000;
-}
-
-function sendMessage(){
-	$('#sayit-button').get(0).click();   
-}
-
-$('#sayit-button').click(function(){
-	alert('buttonsimilation works');   
-});
